c05b508c38ccf9f3067c85fe28720b4ceb4ae83d
sajedul <sajedulkhairul@gmail.com>
2021-05-28 23:58:44 +0600

    First Commit

--- resources/views/home/headerFooter.blade.php
+++ resources/views/home/headerFooter.blade.php
@@ -227,7 +227,7 @@
             </div>
         </div>
 
-    <p class="m-0 text-center text-white">Copyright &copy; AGROBID 2020</p>
+    <p class="m-0 text-center text-white">Copyright &copy; Zulkar & Sajedul 2021</p>
     <!-- Message -->
     <!-- <button class="fixed-bottom btn btn-success float-right " data-toggle="modal" data-target="#messageModal">Feedback here</button> -->
 
